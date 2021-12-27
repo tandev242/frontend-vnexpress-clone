21887c55dbc964e6d1aa7a20a14aa8438062bab3
Tan Dev Zai <superjunior242@gmail.com>
2021-12-27 10:18:29 +0700

    bug: highlight text

--- src/components/skeletons/SingleNewsCard.js
+++ src/components/skeletons/SingleNewsCard.js
@@ -2,44 +2,92 @@ import React, { useState, useEffect } from "react"
 import moment from "moment"
 import Popover from "../includes/Popover"
 
+var key = 0
+
+const rangeToObj = (range) => {
+    return {
+        startKey: range.startContainer.parentNode.dataset.key,
+        startTextIndex: Array.prototype.indexOf.call(
+            range.startContainer.parentNode.childNodes,
+            range.startContainer
+        ),
+        endKey: range.endContainer.parentNode.dataset.key,
+        endTextIndex: Array.prototype.indexOf.call(
+            range.endContainer.parentNode.childNodes,
+            range.endContainer
+        ),
+        startOffset: range.startOffset,
+        endOffset: range.endOffset,
+        color: colors[Math.floor(Math.random() * colors.length)]
+    }
+}
+const objToRange = (rangeStr) => {
+    let range = document.createRange()
+    range.setStart(
+        document.querySelector('[data-key="' + rangeStr.startKey + '"]')
+            .childNodes[rangeStr.startTextIndex],
+        rangeStr.startOffset
+    )
+    range.setEnd(
+        document.querySelector('[data-key="' + rangeStr.endKey + '"]')
+            .childNodes[rangeStr.endTextIndex],
+        rangeStr.endOffset
+    )
+    return range
+}
+
+const colors = [
+    "#FDCF76",
+    "#F6A7C1",
+    "#89AEB2",
+    "#F1CDB0",
+    "#ECAD8F",
+    "#C1CD97",
+    "#E18D96",
+    "#909090",
+    "#9799BA",
+]
+
+const getHighLight = () => {
+    document.designMode = "on"
+    var selArr = [{
+        "startKey": "155",
+        "startTextIndex": 0,
+        "endKey": "155",
+        "endTextIndex": 0,
+        "startOffset": 24,
+        "endOffset": 323,
+        "color": "#9799BA"
+    }]
+    var sel = getSelection()
+    selArr.forEach(function (each) {
+        console.log(objToRange(each))
+        sel.removeAllRanges()
+        sel.addRange(objToRange(each))
+        document.execCommand("hiliteColor", false, each.color)
+    })
+    document.designMode = "off"
+}
+
+
 export const SingleNewsCard = (props) => {
     const { news, setShowAddTopic } = props
     const [position, setPosition] = useState({})
     const [showPopup, setShowPopup] = useState(false)
     const [selectedArray, setSelectedArray] = useState([])
 
-    const colors = [
-        "#FDCF76",
-        "#F6A7C1",
-        "#89AEB2",
-        "#F1CDB0",
-        "#ECAD8F",
-        "#C1CD97",
-        "#E18D96",
-        "#909090",
-        "#9799BA",
-    ]
-
-    var key = 0
-
-
-    const rangeToObj = (range) => {
-        return {
-            startKey: range.startContainer.parentNode.dataset.key,
-            startTextIndex: Array.prototype.indexOf.call(
-                range.startContainer.parentNode.childNodes,
-                range.startContainer
-            ),
-            endKey: range.endContainer.parentNode.dataset.key,
-            endTextIndex: Array.prototype.indexOf.call(
-                range.endContainer.parentNode.childNodes,
-                range.endContainer
-            ),
-            startOffset: range.startOffset,
-            endOffset: range.endOffset,
-            color: colors[Math.floor(Math.random() * colors.length)]
+    useEffect(() => {
+        const addKey = (element) => {
+            if (element.children.length > 0) {
+                Array.prototype.forEach.call(element.children, function (each, i) {
+                    each.dataset.key = key++
+                    addKey(each)
+                })
+            }
         }
-    }
+        addKey(document.body)
+    }, [])
+
 
     const handleSelectedText = (e) => {
         const selectedText = window.getSelection()
@@ -48,20 +96,22 @@ export const SingleNewsCard = (props) => {
         var clientX = clientRect.left
         var clientY = clientRect.top
         const left = e.pageX - clientX - document.documentElement.scrollLeft + 30
-        const top = e.pageY - clientY - document.documentElement.scrollTop + 50
+        const top = e.pageY - clientY - document.documentElement.scrollTop + 80
         // -----------------
         let range
-        if (selectedText.toString().trim()) {
+        if (document.selection) {
+            range = document.selection.createRange();
+        } else if (selectedText.toString().trim() != "") {
             range = selectedText.getRangeAt(0)
-            setSelectedArray([...selectedArray, rangeToObj(range)])
-            console.log(rangeToObj)
-            setShowPopup(true)
-            setPosition({ top, left })
         }
         else {
             setShowPopup(false)
             setShowAddTopic(false)
+            return;
         }
+        setSelectedArray([...selectedArray, rangeToObj(range)])
+        setShowPopup(true)
+        setPosition({ top, left })
     }
 
     const handleAddTopic = () => {
@@ -71,50 +121,10 @@ export const SingleNewsCard = (props) => {
         document.designMode = "off"
     }
 
-
-
-    const objToRange = (rangeStr) => {
-        let range = document.createRange()
-        range.setStart(
-            document.querySelector('[data-key="' + rangeStr.startKey + '"]')
-                .childNodes[rangeStr.startTextIndex],
-            rangeStr.startOffset
-        )
-        range.setEnd(
-            document.querySelector('[data-key="' + rangeStr.endKey + '"]')
-                .childNodes[rangeStr.endTextIndex],
-            rangeStr.endOffset
-        )
-        return range
-    }
-
-    const getHighLight = () => {
-        document.designMode = "on"
-        var selArr = JSON.parse('[{"startKey":"85","startTextIndex":0,"endKey":"85","endTextIndex":0,"startOffset":180,"endOffset":206,"color":"#C1CD97"}]');
-        var sel = getSelection()
-        selArr.forEach(function (each) {
-            console.log(each)
-            sel.removeAllRanges()
-            sel.addRange(objToRange(each))
-            document.execCommand("hiliteColor", false, each.color)
-        })
-        document.designMode = "off"
-    }
-
-    const addKey = (element) => {
-        if (element.children.length > 0) {
-            Array.prototype.forEach.call(element.children, function (each, i) {
-                each.dataset.key = key++
-                addKey(each)
-            })
-        }
-    }
-
-    addKey(document.body)
-
     return (
         <>
             <div className="entity_title">
+                <button type="button" onClick={getHighLight}>get Highlight</button>
                 <h1>
                     <a href="#">{news.title}</a>
                 </h1>
@@ -147,5 +157,4 @@ export const SingleNewsCard = (props) => {
             </div>
         </>
     )
-
 }
